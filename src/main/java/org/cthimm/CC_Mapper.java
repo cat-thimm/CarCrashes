a7ddec64c634b7de53962dc47fea94ec7bbc5548
Caterina <119865119+cat-thimm@users.noreply.github.com>
2024-05-15 10:53:26 +0200

    :tada: reduce mapper inputs :construction:  add new package

--- src/main/java/org/cthimm/CC_Mapper.java
+++ src/main/java/org/cthimm/CC_Mapper.java
@@ -20,8 +20,8 @@ public class CC_Mapper extends MapReduceBase implements Mapper<LongWritable, Tex
         if (fields.length >= 22) { // Ensure all necessary fields are present
             String uniqueId = fields[0];
             String collisionId = fields[1];
-            String crashDate = fields[2];
-            String crashTime = fields[3];
+            //String crashDate = fields[2]; take that info from the other table
+            // String crashTime = fields[3]; take that info from the other table
             String personId = fields[4];
             String personType = fields[5];
             String personInjury = fields[6];
@@ -32,20 +32,20 @@ public class CC_Mapper extends MapReduceBase implements Mapper<LongWritable, Tex
             String bodilyInjury = fields[11];
             String positionInVehicle = fields[12];
             String safetyEquipment = fields[13];
-            String pedLocation = fields[14];
-            String pedAction = fields[15];
+            // String pedLocation = fields[14]; usually empty - only for pedestrians
+            // String pedAction = fields[15]; usually empty - only for pedestrians
             String complaint = fields[16];
             String pedRole = fields[17];
-            String contributingFactor1 = fields[18];
-            String contributingFactor2 = fields[19];
+            // String contributingFactor1 = fields[18]; empty or unspecified
+            // String contributingFactor2 = fields[19]; empty or unspecified
             String personSex = fields[20];
+
+
 
             // Construct JSON object
             String jsonObject = "{"
                     + "\"unique_id\":\"" + uniqueId + "\","
                     + "\"collision_id\":\"" + collisionId + "\","
-                    + "\"crash_date\":\"" + crashDate + "\","
-                    + "\"crash_time\":\"" + crashTime + "\","
                     + "\"person_id\":\"" + personId + "\","
                     + "\"person_type\":\"" + personType + "\","
                     + "\"person_injury\":\"" + personInjury + "\","
@@ -56,12 +56,8 @@ public class CC_Mapper extends MapReduceBase implements Mapper<LongWritable, Tex
                     + "\"bodily_injury\":\"" + bodilyInjury + "\","
                     + "\"position_in_vehicle\":\"" + positionInVehicle + "\","
                     + "\"safety_equipment\":\"" + safetyEquipment + "\","
-                    + "\"ped_location\":\"" + pedLocation + "\","
-                    + "\"ped_action\":\"" + pedAction + "\","
                     + "\"complaint\":\"" + complaint + "\","
                     + "\"ped_role\":\"" + pedRole + "\","
-                    + "\"contributing_factor_1\":\"" + contributingFactor1 + "\","
-                    + "\"contributing_factor_2\":\"" + contributingFactor2 + "\","
                     + "\"person_sex\":\"" + personSex + "\""
                     + "}";
 
