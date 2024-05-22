f57646d7b0954743cafa7b29d538d669136750a7
Caterina <119865119+cat-thimm@users.noreply.github.com>
2024-05-22 12:12:52 +0200

    change hadoop job

--- run-hadoop-job.sh
+++ run-hadoop-job.sh
@@ -2,18 +2,21 @@
 set -e
 set -x
 
-if [ ! -d "/input" ]; then
-  mkdir /input
-fi
 
-cp /raw_data/persons.csv /input/persons.csv
-hadoop fs -mkdir -p /input
-hdfs dfs -put /input/persons.csv /input
+# Check if the /input directory exists in HDFS and create it if not
+# hadoop fs -mkdir -p /input
 
-hadoop jar /opt/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.2.1.jar \
+# Check if persons.csv exists in HDFS and remove it if it does
+#hadoop fs -test -e /input/persons.csv && hadoop fs -rm /input/persons.csv
+
+# Put the local persons.csv file into HDFS
+# hdfs dfs -put /input/persons.csv /input
+
+# Run the Hadoop streaming job
+hadoop jar /opt/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.2.0.jar \
   -file /mapper.py \
   -mapper /mapper.py \
   -file /reducer.py \
   -reducer /reducer.py \
-  -input /input/persons.csv \
+  -input /raw_data/persons.csv \
   -output /output
